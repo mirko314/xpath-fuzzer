c98b2dfbda88e795e7a1a71924c90da1714edb47
Mirko Budszuhn <mirko314@users.noreply.github.com>
2019-04-07 15:57:08 +0200

    Fix REXML Namespaces

--- libraries/rexml/rexml.rb
+++ libraries/rexml/rexml.rb
@@ -7,10 +7,10 @@ with_example_namespaces = true
 file = File.new( file_name )
 @doc = REXML::Document.new file
 namespaces = with_example_namespaces ? {
-  'a': "https://a.com/",
-  'b': "https://b.com/",
-  'c': "https://c.com/",
-  'd': "https://d.com/",
-  'e': "https://e.com/"
+  'a'=> "https://a.com/",
+  'b'=> "https://b.com/",
+  'c'=> "https://c.com/",
+  'd'=> "https://d.com/",
+  'e'=> "https://e.com/"
 }: nil
-puts XPath.match( @doc, xpath, namespaces=namespaces )
+puts XPath.match( @doc, xpath, namespaces)
